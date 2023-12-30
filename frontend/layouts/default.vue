0e01b5740942bb5b2d17f4a4b25f43d18fba2bb7
CLL <140147262+CLcosep@users.noreply.github.com>
2023-12-30 20:28:25 +0800

    I am sleepy

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,10 +2,11 @@
     <div>
         <header class="shadow-sm bg-white">
             <nav class="container mx-auto p-4 flex justify-between">
-                <NuxtLink to="/" class="font-bold text-3xl text-pink-300">ToDoList</NuxtLink>
+                <NuxtLink to="/" class="font-bold text-3xl text-[#beabed]">ToDoList</NuxtLink>
                 <ul class="flex gap-5">
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/authentication/register" class="bg-[#beabed] text-white px-3 py-2 rounded-md text-white">Join now</NuxtLink></li>
                 </ul>
             </nav>
         </header>
@@ -20,6 +21,8 @@
 
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+    .router-link-exact-active {
+        color: #beabed;
+    }
 </style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,21 @@
 <template>
-    <h1 class="">To Do List</h1>
-    <form @submit.prevent="addTodo">
-        <input v-model="newTodo">
-        <button class="btn ml-4"> Add to List </button>
-    </form>
-    <ul>
-        <li v-for="todo in todos" :key="todo.id">
-        <input type="checkbox" v-model="todo.done" class="mr-2">
-        <span :class="{done: todo.done}"> {{ todo.text }}</span>
-        <button @click="removeTodo(todo)" class="btn ml-4 mb-1">X</button>
-        </li>
-    </ul>
-    <button @click="hideTodo = !hideTodo">
-        {{ hideTodo ? 'Show all' : 'Hide Completed' }}
-    </button>
+    <div class="container">
+        
+        <div class="display-box w-90 max-w-450 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-50 md:p-60 lg:p-70 text-center">
+            <h1 class="justify-center ">To Do List</h1>
+            <form @submit.prevent="addTodo">
+                <input v-model="newTodo">
+                <button class="btn ml-4"> Add to List </button>
+            </form>
+            <ul>
+                <li v-for="todo in todos" :key="todo.id">
+                <input type="checkbox" v-model="todo.done" class="mr-2">
+                <span :class="{done: todo.done}"> {{ todo.text }}</span>
+                <button @click="removeTodo(todo)" class="btn ml-4 mb-1">X</button>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -22,14 +24,7 @@
     let id = 0
     //
     const newTodo = ref('')
-    const hideTodo = ref(false)
     const todos = ref([])
-
-    const filteredTodos = computed(() => {
-  return hideTodo.value
-    ? todos.value.filter((t) => !t.done)
-    : todos.value
-})
 
     function addTodo(){
         todos.value.push({id: id++, text: newTodo.value, done: false})
@@ -49,8 +44,5 @@
 
     html{
         background-color: black;
-    }
-    .btn {
-        @apply bg-[#beabed] text-white px-3 py-2  rounded-md text-sm text-white;
     }
 </style>
